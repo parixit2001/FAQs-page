b8aa285ed8681d7a08b133284074f42fc904b5f3
parixit2001 <73827493+parixit2001@users.noreply.github.com>
2022-02-12 13:46:59 +0530

    Add files via upload

---
+++ FAQ.js
@@ -0,0 +1,25 @@
+ const faqs= document.querySelectorAll(".faq");
+
+ faqs.forEach((faq) => {
+  faq.addEventListener("click" , () =>{
+   faq.classList.toggle("active");
+ });
+});
+
+function myFunction() {
+{
+    let form = document.getElementById("form__submit");
+    form.submit();
+    alert("submited");
+}
+}
+
+function myFunctions() {
+  {
+      let form = document.getElementById("form__submit");
+      form.submit();
+      alert("submited");
+  }
+  }
+
+ 
